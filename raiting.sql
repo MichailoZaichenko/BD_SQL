562ced1b59fe4bb635265b3ba0623cdf6504187d
MichailoZaichenko <michailo.zaichenko@gmail.com>
2023-01-17 22:20:30 +0200

    add peoples in raiting

--- raiting.sql
+++ raiting.sql
@@ -9,3 +9,15 @@ CREATE TABLE "Raiting" (
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
 
+INSERT INTO "Raiting" (Second_name, First_name, Patronymic, Points, Gems) VALUES ("Ярошкін",  "Сергій", "Сергійович", 491, 200);
+INSERT INTO "Raiting" (Second_name, First_name, Patronymic, Points, Gems) VALUES ("Таран", "Андрій", "Ігорович", 481, 140);
+INSERT INTO "Raiting" (Second_name, First_name, Patronymic, Points, Gems) VALUES ("Бєлоусов", "Юрій", "Володимирович",423, 120);
+INSERT INTO "Raiting" (Second_name, First_name, Patronymic, Points, Gems) VALUES ("Зайченко", "Михайло", "Андрійович", 379, 70);
+INSERT INTO "Raiting" (Second_name, First_name, Patronymic, Points, Gems) VALUES ("Марченко","Ілля","Андрійович", 379, 34);
+INSERT INTO "Raiting" (Second_name, First_name, Patronymic, Points, Gems) VALUES ("Лозовий","Олексій","Андрійович",277, 56);
+INSERT INTO "Raiting" (Second_name, First_name, Patronymic, Points, Gems) VALUES ("Ахмедов","Ахмед","Анар Огли",263, 98);
+INSERT INTO "Raiting" (Second_name, First_name, Patronymic, Points, Gems) VALUES ("Діденко","Нікіта","Сергійович",259, 76);
+INSERT INTO "Raiting" (Second_name, First_name, Patronymic, Points, Gems) VALUES ("Рыжков","Владислав","Андреевич",222, 55);
+INSERT INTO "Raiting" (Second_name, First_name, Patronymic, Points, Gems) VALUES ("Каштаєв","Артур","Віталійович",220, 90);
+INSERT INTO "Raiting" (Second_name, First_name, Patronymic, Points, Gems) VALUES ("Тараканов","Сергiй","Михайлович",185, 80);
+INSERT INTO "Raiting" (Second_name, First_name, Patronymic, Points, Gems) VALUES ("Стрельченко","Дмитро","Олександрович",142, 60);
