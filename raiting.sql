dfdf55e35435fb3fa1e3676336af29a741136e93
MichailoZaichenko <michailo.zaichenko@gmail.com>
2023-01-17 22:19:38 +0200

    ADD TABLE Raiting

---
+++ raiting.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS "Raiting";
+CREATE TABLE "Raiting" (
+	"id"	INTEGER,
+	"Second_name"	TEXT,
+	"First_name"	TEXT,
+	"Patronymic"	TEXT,
+	"Points"	INTEGER,
+	"Gems"	INTEGER,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+
